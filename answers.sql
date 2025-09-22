651ec2412a4fc9e91f663d6fbdd0eb8ce60e4145
Lidiya Bokona <mystery.written@gmail.com>
2025-09-22 04:50:35 +0300

    answer for question number 2

--- answers.sql
+++ answers.sql
@@ -1,4 +1,5 @@
 USE salesdb;
+-- Question 1: INNER JOIN employees with offices
 SELECT 
     e.firstName, 
     e.lastName, 
@@ -6,3 +7,10 @@ SELECT
     e.officeCode
 FROM employees e
 INNER JOIN offices o ON e.officeCode = o.officeCode;
+-- Question 2: LEFT JOIN products with productlines
+SELECT 
+    p.productName, 
+    p.productVendor, 
+    p.productLine
+FROM products p
+LEFT JOIN productlines pl ON p.productLine = pl.productLine;
